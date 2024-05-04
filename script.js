123984eb896e1892aad281077437c1b835236baf
darlyson <jdarlyson@gmail.com>
2024-05-03 23:19:59 -0300

    adicionado icones do github e linkedIn. mudado o nome do footer,

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("downloadButton").addEventListener("click", function() {    
+    var link = document.createElement('a');
+    // Defina o atributo href com o link para o arquivo PDF
+    link.href = 'assets/Curriculo.pdf'; // Substitua pelo caminho real do seu arquivo PDF
+    // Defina o atributo download como o nome do arquivo que deseja que seja baixado
+    link.download = 'Curriculo.pdf'; // Substitua pelo nome que deseja para o arquivo
+    // Simule um clique no link para iniciar o download
+    link.click();
+});
+
