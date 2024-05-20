fc5fa64ed39264424005b6e3a96ee5096d37ceee
darlyson <jdarlyson@gmail.com>
2024-05-20 14:08:36 -0300

    carrossel no turismo

---
+++ Projetos/Turismo/turismo.js
@@ -0,0 +1,50 @@
+// carrossel
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper1 = new Swiper('.swiper1', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+    const swiper2 = new Swiper('.swiper2', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+    const swiper3 = new Swiper('.swiper3', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+});
